2d851075f84e2ba8ea4a2f0145834b6eea04e73c
jackgu <gestoru@1thefull.com>
2018-11-08 13:10:06 +0900

    add nodejs boilerplate

---
+++ nodejs-boilerplate/app.js
@@ -0,0 +1,48 @@
+var express  = require('express'),
+    compress = require('compression'),
+    hbs      = require('hbs'),
+    moment   = require('moment'),
+    router   = require(__dirname + '/routes').router,
+    app      = express(),
+    error    = require(__dirname + '/middleware/error');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('dateFormat', function(context, block) {
+    var f = block.hash.format || "MMM DD, YYYY hh:mm:ss A";
+    return moment(context).format(f);
+});
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views/pages');
+app.engine('html', hbs.__express);
+
+app.use(compress({
+    filter: function(req, res) {
+        return (/json|text|javascript|css|image\/svg\+xml|application\/x-font-ttf/).test(res.getHeader('Content-Type'));
+    },
+    level: 9
+}));
+
+if (app.get('env') === 'development'){
+    app.use(express.static(__dirname + '/public', {maxAge: 86400000}));
+}
+
+var route = express.Router();
+
+route.get('/index.html', function(req, res){
+    res.redirect(301, '/');
+});
+route.get('/', router.index);
+route.get('/projects.html', router.projects);
+route.get('/services.html', router.services);
+route.get('/downloads.html', router.downloads);
+route.get('/about.html', router.about);
+route.get('/contact.html', router.contact);
+
+app.use('/', route);
+
+app.use(error.notFound);
+app.use(error.serverError);
+
+module.exports = app;
