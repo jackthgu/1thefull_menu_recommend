6a3bbaa967ecb71bf9ef7fb4e6a614df0f02ad0a
jackgu <gestoru@1thefull.com>
2018-11-09 08:54:57 +0900

    add sql file

---
+++ tablequery/csv_to_sql.sh
@@ -0,0 +1,1 @@
+csvsql --dialect mysql --snifflimit 100000 bigdatafile.csv > maketable.sql
--- tablequery/mysql_query.py
+++ tablequery/mysql_query.py
@@ -3,6 +3,12 @@ import mysql.connector
 from mysql.connector import errorcode
 import pdb
 
+def print_cursor(cnx):
+    cursor=cnx
+    row = cursor.fetchone()
+    while row is not None:
+        print(row)
+        row = cursor.fetchone()
 
 def init_session() :
     try:
@@ -12,6 +18,7 @@ def init_session() :
                 password="asdf;lkj",
                 database="food"
                 )
+        return our_db
 
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -28,14 +35,29 @@ def single_select(cnx,sel,frm):
     cursor = cnx.cursor()
     query = "SELECT {0} FROM {1} ".format(sel,frm)
     print(query)
+    cursor.execute(query)
+    print_cursor(cursor)
 
 
 def get_db_all(cnx):
     cursor = cnx.cursor()
     query = "SELECT * FROM fooddata"
-    iterator = cursor.execute(query, params=None, multi=True)
-    pdb.set_trace()
+    cursor.execute("SELECT * FROM fooddata")
+    print_cursor(cursor)
 
+def get_column_name(cnx,tablename):
+    cursor = cnx.cursor()
+    query = "SELECT `COLUMN_NAME`  FROM `INFORMATION_SCHEMA`.`COLUMNS`  WHERE `TABLE_SCHEMA`='food'      AND `TABLE_NAME`='{0}'".format(tablename)
+    cursor.execute(query)
+    print_cursor(cursor)
+
+def get_column_type(cnx,tablename,column):
+    cursor = cnx.cursor()
+    query = "SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS  WHERE table_name = '{0}' AND COLUMN_NAME = '{1}'".format(tablename,column)
+    cursor.execute(query)
+    print_cursor(cursor)
     
 aa = init_session()
-get_db_all(aa)
+get_column_type(aa,"fooddata","year")
+
+
